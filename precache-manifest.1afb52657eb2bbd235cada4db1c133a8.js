30cf2bf2ba82a43cb87483d2198a39fe8c8ed49f
Francesca Giannino <francesca.giannino86@gmail.com>
2020-09-13 00:15:20 +0200

    Updates

---
+++ precache-manifest.1afb52657eb2bbd235cada4db1c133a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af801a37540c3f2ee65778f75c8e73c1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "76de502ede29f3c7301b",
+    "url": "/monsters-rolodex/static/css/main.a507d800.chunk.css"
+  },
+  {
+    "revision": "62b0e343484d6448e8d5",
+    "url": "/monsters-rolodex/static/js/2.fa8709d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/monsters-rolodex/static/js/2.fa8709d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76de502ede29f3c7301b",
+    "url": "/monsters-rolodex/static/js/main.2b489422.chunk.js"
+  },
+  {
+    "revision": "94bc6a680a303c7f6d62",
+    "url": "/monsters-rolodex/static/js/runtime-main.85c7751d.js"
+  }
+]);
